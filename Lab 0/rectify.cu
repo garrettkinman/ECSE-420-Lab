9542940ad3ed6a6d2b1ce5b71290cc3fa588897d
garrettkinman <garrettkinman@gmail.com>
2020-10-05 00:23:58 -0400

    completed main (mostly)
    need to test/debug, and add timings

--- Lab 0/rectify.cu
+++ Lab 0/rectify.cu
@@ -4,10 +4,10 @@
 
 #include <stdio.h>
 
-__global__ void rectifyParallel(unsigned char* original_img, unsigned char* new_img, unsigned int num_thread, unsigned int size)
+__global__ void rectifyParallel(unsigned char* original_img, unsigned char* new_img, unsigned int num_threads, unsigned int img_size)
 {
     // iterate through all the blocks, same threadIdx for each
-    for (int i = threadIdx.x; i < size; i += num_thread) {
+    for (int i = threadIdx.x; i < img_size; i += num_threads) {
         if (original_img[i] < 127) {
             new_img[i] = 127;
         }
@@ -17,14 +17,14 @@ __global__ void rectifyParallel(unsigned char* original_img, unsigned char* new_
     }
 }
 
-void rectifySequential(unsigned char* og_img, unsigned char* new_img, unsigned int num_thread, unsigned int size) {
+void rectifySequential(unsigned char* original_img, unsigned char* new_img, unsigned int img_size) {
     // iterate through all elements of og_img
-    for (int i = 0; i < size; i++) {
-        if (og_img[i] < 127) {
+    for (int i = 0; i < img_size; i++) {
+        if (original_img[i] < 127) {
             new_img[i] = 127;
         }
         else {
-            new_img[i] = og_img[i];
+            new_img[i] = original_img[i];
         }
     }
 }
@@ -63,11 +63,11 @@ int main(int argc, char *argv[]) {
 
     unsigned char* original_img;
     unsigned char* new_img;
-    unsigned int width_in, height_in;
+    unsigned int img_width, img_height;
 
-    int error = lodepng_decode32_file(&original_img, &width_in, &height_in, input_filename);
+    int error = lodepng_decode32_file(&original_img, &img_width, &img_height, input_filename);
     if (error) {
-        printf("Error %u: %s\n", error, lodepng_error_text(error));
+        printf("Error %d: %s\n", error, lodepng_error_text(error));
         return -1;
     }
 
@@ -79,7 +79,7 @@ int main(int argc, char *argv[]) {
     unsigned char* new_img_cuda;
 
     // allocate for CPU
-    unsigned int img_size = width_in * height_in * 4 * sizeof(unsigned char);
+    unsigned int img_size = img_width * img_height * 4 * sizeof(unsigned char);
     new_img = (unsigned char*)malloc(img_size);
 
     // allocate for GPU
@@ -87,21 +87,43 @@ int main(int argc, char *argv[]) {
     cudaMalloc((void**)&new_img_cuda, img_size);
     cudaMemcpy(original_img, original_img, img_size, cudaMemcpyHostToDevice);
 
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // step 4: call parallelized rectify function, record performance
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+    // TODO: measure time
+    rectifyParallel<<<1, num_threads>>>(original_img_cuda, new_img_cuda, num_threads, img_size);
 
+    cudaDeviceSynchronize();
+    cudaMemcpy(new_img, new_img_cuda, img_size, cudaMemcpyHostToDevice);
 
-    // TODO:
-    // 1) read in and validate arguments
-    // 2) load in input png from file
-    // 3) make variables available to both CPU and GPU
-    // 4) specify launch config of kernel function
-    // 5) call parallelized rectify function, record performance
-    // 6) call sequential rectify function, record performance
-    // 7) write output image from parallelized rectify function to file
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // step 5: write output image from parallelized rectify function to file
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+    error = lodepng_encode32_file(output_filename, new_img, img_width, img_height);
+    if (error) {
+        printf("Error %d: %s\n", error, lodepng_error_text(error));
+        return -1;
+    }
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // step 6: call sequential rectify function, record performance
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    // TODO: measure time
+    rectifySequential(original_img, new_img, img_size);
+
+    // ~~~~~~~~~~~~~~~~~~~~~
+    // step 7: free at last!
+    // ~~~~~~~~~~~~~~~~~~~~~
 
     free(input_filename);
     free(output_filename);
+    free(original_img);
+    free(new_img);
+    cudaFree(original_img_cuda);
+    cudaFree(new_img_cuda);
 
     return 0;
 }
