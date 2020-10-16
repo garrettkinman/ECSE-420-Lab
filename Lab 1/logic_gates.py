e5b66402b4b3dbeb7e08a4b6d0ec4313f9f30e37
garrettkinman <garrettkinman@gmail.com>
2020-10-16 17:42:17 -0400

    changed to CLI args
    outputs wrong text, need to debug

--- Lab 1/logic_gates.py
+++ Lab 1/logic_gates.py
@@ -1,4 +1,5 @@
 # imports
+import sys
 import enum
 import numpy as np
 import pandas as pd
@@ -7,21 +8,24 @@ import pycuda.autoinit
 from pycuda.compiler import SourceModule
 
 # take in program params
-execution_type = int(input("What kind of execution type do you want?\n"
-                        "Enter '1' for sequential.\n"
-                        "Enter '2' for parallel explicit.\n"
-                        "Enter '3' for parallel unified.\n"))
-input_filepath = input("Input file path: ")
-input_filelength = int(input("Input file length: "))
-output_filepath = input("Output file path: ")
+# TODO: turn these into CLI args
+execution_type = sys.argv[1]
+input_filepath = sys.argv[2]
+input_length = int(sys.argv[3])
+output_filepath = sys.argv[4]
+# execution_type = int(input("What kind of execution type do you want?\n"
+#                         "Enter '1' for sequential.\n"
+#                         "Enter '2' for parallel explicit.\n"
+#                         "Enter '3' for parallel unified.\n"))
+# input_filepath = input("Input file path: ")
+# input_filelength = int(input("Input file length: "))
+# output_filepath = input("Output file path: ")
 
 # validate params
 # TODO
 
 # read in inputs, convert to numpy array
-gate_inputs = pd.read_csv(input_filepath, header=None).values
-# TODO: trim size to input_filelength
-
+gate_inputs = pd.read_csv(input_filepath, header=None).values[:input_length]
 
 class Gates(enum.Enum):
     AND = 0
@@ -62,3 +66,16 @@ def simulate_parallel_unified(gates):
     # TODO: same as above, but parallel with unified cuda memory allocation
     return None
 
+def write_to_output():
+    if execution_type == "sequential":
+        simulate_sequential(gate_inputs).tofile(output_filepath, sep='\n')
+    elif execution_type == "parallel_explicit":
+        pd.DataFrame(simulate_parallel_explicit(gate_inputs)).to_csv(output_filepath)
+    elif execution_type == "parallel_unified":
+        pd.DataFrame(simulate_parallel_unified(gate_inputs)).to_csv(output_filepath)
+    else:
+        # ? throw exception?
+        return
+
+# perform simulations
+write_to_output()
