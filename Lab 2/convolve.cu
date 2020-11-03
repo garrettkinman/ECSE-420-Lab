e8a89a09a63e6c4c29479cd3774045fa56367aa0
garrettkinman <garrettkinman@gmail.com>
2020-11-03 17:43:04 -0500

    sequential and parallel works

--- Lab 2/convolve.cu
+++ Lab 2/convolve.cu
@@ -7,13 +7,6 @@
 #include <time.h>
 #include <math.h>
 
-typedef struct Image {
-    unsigned char* img;
-    unsigned int size;
-    unsigned int width;
-    unsigned int height;
-} Image;
-
 unsigned char clampedWeightedSum(unsigned char* img, float* weights, unsigned width, unsigned start, unsigned color) {
     // perform convolution
     float sum = 0.0;
@@ -31,7 +24,7 @@ unsigned char clampedWeightedSum(unsigned char* img, float* weights, unsigned wi
     }
 
     // round and cast
-    return (unsigned char)roundf(sum);
+    return (unsigned char)sum;
 }
 
 __device__ unsigned char clampedWeightedSumKernel(unsigned char* img, float* weights, unsigned width, unsigned start, unsigned color) {
@@ -72,28 +65,22 @@ __global__ void convolveParallel(unsigned char* original, unsigned char* convolv
     }
 }
 
-//void convolveSequential(Image* original, Image* convolved) {
-//    // increment by 4 values, as each pixel has 4 channels: RGBA
-//    int increment = 4 * sizeof(unsigned char);
-//    // k is index in the output image
-//    int k = 0;
-//    // width is in bytes, original->width is in pixels
-//    int height = 4 * original->width * sizeof(unsigned char);
-//    for (int i = 0; i < original->size; i += increment) {
-//        // if outermost pixel of original image, correct k
-//        // where image is row-major (TODO: make sure it is actually row-major and not column-major)
-//        if (i % height == 0 || (i + increment) % height == 0 || i - height < 0 || i + height > original->size) {
-//            k -= increment;
-//        }
-//        else {
-//            // else, convolve with weight matrix (don't need to convolve alpha channel)
-//            for (int j = 0; j < 3 * sizeof(unsigned char); j += sizeof(unsigned char)) {
-//                convolved->img[k + j] = clampedWeightedSum(weights, original, i, j);
-//            }
-//        }
-//
-//    }
-//}
+void convolveSequential(unsigned char* original, unsigned char* convolved, float* weights, unsigned width, unsigned height) {
+    int n_windows = (width - 2) * (height - 2);
+    for (int i = 0; i < n_windows; i++) {
+        int window_start = (i / (width - 2)) * (width) + (i % (width - 2));
+        for (int color = 0; color < 4; color++) {
+            float result = 0.0;
+            if (color == 3) {
+                result = 255;
+            }
+            else {
+                result = clampedWeightedSum(original, weights, width, window_start, color);
+            }
+            convolved[i * 4 + color] = result;
+        }
+    }
+}
 
 int main(int argc, char* argv[]) {
 
@@ -117,8 +104,6 @@ int main(int argc, char* argv[]) {
     // step 2: read in input image from file
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    //Image original;
-    //Image convolved;
     unsigned char* original;
     unsigned char* convolved;
     unsigned width, height;
@@ -132,14 +117,6 @@ int main(int argc, char* argv[]) {
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // step 3: allocate for CPU and GPU
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-    //Image original_cuda;
-    //Image convolved_cuda;
-
-    // allocate for CPU
-    //original.size = original.width * original.height * 4 * sizeof(unsigned char);
-    //convolved.size = (original.width - 2) * (original.height - 2) * 4 * sizeof(unsigned char);
-    //convolved.img = (unsigned char*)malloc(convolved.size);
 
     // allocate for CPU
     size_t original_size = width * height * 4 * sizeof(unsigned char);
@@ -159,28 +136,41 @@ int main(int argc, char* argv[]) {
     cudaMemcpy(convolved_cuda, convolved, convolved_size, cudaMemcpyHostToDevice);
     cudaMemcpy(w_cuda, w[0], 3 * 3 * sizeof(float), cudaMemcpyHostToDevice);
 
-
-    // allocate for GPU
-    //cudaMalloc((void**)&(original_cuda.img), original.size);
-    //cudaMalloc((void**)&(convolved_cuda.img), convolved.size);
-    //cudaMalloc((void**)&original_cuda, sizeof(original));
-    //cudaMalloc((void**)&convolved_cuda, sizeof(convolved));
-    //cudaMemcpy(&original_cuda, &original, original.size, cudaMemcpyHostToDevice);
-    //cudaMemcpy(original_cuda.img, original.img, original.size, cudaMemcpyHostToDevice);
-
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // step 6: convolve sequentially, record performance
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    //convolveSequential(&original, &convolved);
-    //error = lodepng_encode32_file(argv[2], convolved.img, convolved.width, convolved.height);
+    clock_t startCPU = clock();
+    convolveSequential(original, convolved, w[0], width, height);
+    clock_t stopCPU = clock();
+
+    printf("Sequential: %.6f ms\n", ((double)stopCPU - (double)startCPU) * 1000.0 / CLOCKS_PER_SEC);
 
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // step 7: convolve in parallel, record performance
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     unsigned windows_per_thread = ((width - 2) * (height - 2)) / n_threads;
+
+    cudaEvent_t startGPU, stopGPU;
+    cudaEventCreate(&startGPU);
+    cudaEventCreate(&stopGPU);
+
+    cudaEventRecord(startGPU);
     convolveParallel<<<1, n_threads>>>(original_cuda, convolved_cuda, w_cuda, width, height, n_threads, windows_per_thread);
+    cudaEventRecord(stopGPU);
+    cudaEventSynchronize(stopGPU);
+
+    float timeGPU;
+    cudaEventElapsedTime(&timeGPU, startGPU, stopGPU);
+    printf("Parallel Explicit: %.6f ms\n", timeGPU);
+
+    cudaEventDestroy(startGPU);
+    cudaEventDestroy(stopGPU);
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // step 8: write convolved image to file
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     cudaMemcpy(convolved, convolved_cuda, convolved_size, cudaMemcpyDeviceToHost);
     
@@ -199,4 +189,6 @@ int main(int argc, char* argv[]) {
     cudaFree(original_cuda);
     cudaFree(convolved_cuda);
     cudaFree(w_cuda);
+
+    return 0;
 }
