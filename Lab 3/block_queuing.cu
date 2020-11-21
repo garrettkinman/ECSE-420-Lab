5e57c978da709dc70655cf928bc698d01268c340
Ahmed Azhar <ahmedazhar@Ahmeds-MacBook-Pro.local>
2020-11-21 08:17:30 +0500

    Refactored block_queuing.cu

--- Lab 3/block_queuing.cu
+++ Lab 3/block_queuing.cu
@@ -21,8 +21,8 @@
 /*
 helper functions to read and write data
 */
-int read_input_one_two_four(int** input1, char* filepath);
-int read_input_three(int** input1, int** input2, int** input3, int** input4, char* filepath);
+int read_input(int** input_vals, char* path_inputfile);
+int read_third_file(char* path_inputfile, int** first_input, int** second_input, int** third_input, int** fourth_input);
 void write_data(int* data, int length, char* filepath);
 
 __device__ int numNextLevelNodes = 0;
@@ -61,26 +61,23 @@ __global__ void block_queuing_kernel(int numCurrLevelNodes, int* currLevelNodes,
 
     int threadIndex = threadIdx.x + (blockDim.x * blockIdx.x);
 
-    // Loop over all nodes in current level
+    // Iterate through each node in current level
     for (int id = threadIndex; id < numCurrLevelNodes; id++) {
         int nodeIdx = currLevelNodes[id];      
-
-        // Loop over all neighbors of the node
+        // Iterate through each neighbor of the node
         for (int nId = nodePtrs[nodeIdx]; nId < nodePtrs[nodeIdx+1]; nId++) {          
             int neighborIdx = nodeNeighbors[nId];
-
-            // If the neighbor has not been visited yet
+            // If the neighbor is not visited
             const int visited = atomicExch(&(nodeVisited[neighborIdx]), 1);
             if (!(visited)) {
                 const int queueIdx = atomicAdd(&sharedBlockQueueSize, 1);
-
+                // Solve Gate
                 nodeOutput[neighborIdx] = gate_solver(nodeGate[neighborIdx], nodeOutput[nodeIdx], nodeInput[neighborIdx]);
-
-                // add to block queue if not full
-                // else, add to global queue
-                if (queueIdx < queueSize)
-                    sharedBlockQueue[queueIdx] = neighborIdx;                  
-                else {
+                // if not full add to block queue 
+                if (queueIdx < queueSize){
+                  sharedBlockQueue[queueIdx] = neighborIdx;
+                }                    
+                else { // else, add to global queue
                     sharedBlockQueueSize = queueSize;
                     const int GlIdx = atomicAdd(&numNextLevelNodes, 1);
                     nextLevelNodesQueue[GlIdx] = neighborIdx; 
@@ -90,13 +87,12 @@ __global__ void block_queuing_kernel(int numCurrLevelNodes, int* currLevelNodes,
     }
     
     __syncthreads();
-
-    if (threadIdx.x == 0)
-        blockGlobalQueueIdx = atomicAdd(&numNextLevelNodes, sharedBlockQueueSize);
-  
+    if (threadIdx.x == 0){
+      blockGlobalQueueIdx = atomicAdd(&numNextLevelNodes, sharedBlockQueueSize);
+    } 
     __syncthreads();
 
-    // store block queue in global queue
+    // storing block queue in global queue
     for (int i = threadIdx.x; i < sharedBlockQueueSize; i += blockDim.x)
         nextLevelNodesQueue[blockGlobalQueueIdx + i] = sharedBlockQueue[i];
 }
@@ -106,85 +102,78 @@ int main(int argc, char *argv[]){
     // ~~~~~~~~~~~~~~~~~~~~~~~
     // step 1: parse arguments
     // ~~~~~~~~~~~~~~~~~~~~~~~
+
+    //User Input 
+    const int blockSize = atoi(argv[1]); const int numBlocks = atoi(argv[2]); const int queueSize = atoi(argv[3]);
+    char* input_1 = argv[4]; char* input_2 = argv[5]; char* input_3 = argv[6]; char* input_4 = argv[7];
+    char* output_node = argv[8]; char* output_nln = argv[9];
     
-    if (argc < 10) {
-        printf("Require parameters in the following order: <numBlock> <blockSize> <sharedQueueSize> <path_to_input_1.raw> <path_to_input_2.raw> <path_to_input_3.raw> <path_to_input_4.raw> <output_nodeOutput_filepath> <output_nextLevelNodes_filepath>.\n");
-        exit(1);
-    }
+    // Node Variables
+    int countNodes = 0; int numNodePtrs = 0; int *nodePtrs;
+    int *nodeGate; int *nodeInput; int *nodeOutput;
+    
+    // Node Neighbours Variables
+    int *nodeNeighbors; int countNodeNeighbors = 0; int *nodeVisited; 
 
-    int numNodePtrs;
-    int numNodes;
-    int *nodePtrs_h;
-    int *nodeNeighbors_h;
-    int *nodeVisited_h;
-    int numTotalNeighbors_h;
-    int *currLevelNodes_h;
-    int numCurrLevelNodes;
-    int numNextLevelNodes_h = 0;
-    int *nodeGate_h;
-    int *nodeInput_h;
-    int *nodeOutput_h;
+    //Node level variables
+    int *currLevelNodes; int numCurrLevelNodes = 0; //Current Level
+    int *nextLevelNodes; int numNextLevelNodes = 0; //Next Level
 
-    const int blockSize = atoi(argv[1]);
-    const int numBlocks = atoi(argv[2]);
-    const int queueSize = atoi(argv[3]);
 
-    char* input1 = argv[4];
-    char* input2 = argv[5];
-    char* input3 = argv[6];
-    char* input4 = argv[7];
-
-    char* nodeOutputFilename = argv[8];
-    char* nextLevelNodesFilename = argv[9];
-
+    //Argument Check
+    if (argc != 10) {return fprintf(stderr, "Not Enough or Too Many Arguments!\n");}
+  
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // step 2: read in inputs from file
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    numNodePtrs = read_input_one_two_four(&nodePtrs_h, input1);
-    numTotalNeighbors_h = read_input_one_two_four(&nodeNeighbors_h, input2);
-    numNodes = read_input_three(&nodeVisited_h, &nodeGate_h, &nodeInput_h, &nodeOutput_h, input3);
-    numCurrLevelNodes = read_input_one_two_four(&currLevelNodes_h, input4);
+    countNodes = read_third_file(input_3, &nodeVisited, &nodeGate, &nodeInput, &nodeOutput);
+    numNodePtrs = read_input(&nodePtrs, input_1);
+    countNodeNeighbors = read_input(&nodeNeighbors, input_2);
+    
+    numCurrLevelNodes = read_input(&currLevelNodes, input_4);
 
     // ~~~~~~~~~~~~~~~~~~~~~~~~
     // step 3: allocate for GPU
     // ~~~~~~~~~~~~~~~~~~~~~~~~
 
-    // outputs
-    int *nextLevelNodes_h = (int *)malloc(numNodes*sizeof(int));
-    int *nextLevelNodes_cuda = (int *)malloc(numNodes*sizeof(int));
-    cudaMalloc (&nextLevelNodes_cuda, numCurrLevelNodes * sizeof(int));
-    cudaMemcpy(nextLevelNodes_cuda, nextLevelNodes_h, numCurrLevelNodes * sizeof(int), cudaMemcpyHostToDevice);
-  
-    // copy to device
-    int numNodesSize = numNodes * sizeof(int);
-    int* currLevelNodes_cuda = (int*)malloc(numCurrLevelNodes * sizeof(int)) ; 
-    cudaMalloc (&currLevelNodes_cuda, numCurrLevelNodes * sizeof(int));
-    cudaMemcpy(currLevelNodes_cuda, currLevelNodes_h, numCurrLevelNodes * sizeof(int), cudaMemcpyHostToDevice);
-
-    int* nodeNeighbors_cuda = (int*)malloc(numTotalNeighbors_h * sizeof(int)) ; 
-    cudaMalloc (&nodeNeighbors_cuda, numTotalNeighbors_h * sizeof(int));
-    cudaMemcpy(nodeNeighbors_cuda, nodeNeighbors_h, numTotalNeighbors_h * sizeof(int), cudaMemcpyHostToDevice);
-
+    nextLevelNodes = (int *)malloc(countNodes*sizeof(int));
+    int *nextLevelNodes_cuda = (int *)malloc(countNodes*sizeof(int));
+    int numNodesSize = countNodes * sizeof(int);
+    int* currLevelNodes_cuda = (int*)malloc(numCurrLevelNodes * sizeof(int)); 
+    int* nodeNeighbors_cuda = (int*)malloc(countNodeNeighbors * sizeof(int)); 
     int* nodePtrs_cuda = (int*)malloc(numNodePtrs * sizeof(int)) ; 
+    int* nodeVisited_cuda = (int*)malloc(numNodesSize);
+    int* nodeInput_cuda = (int*)malloc(numNodesSize); 
+    int* nodeOutput_cuda = (int*)malloc(numNodesSize); 
+    int* nodeGate_cuda = (int*)malloc(numNodesSize);
+    
+    // Calling CUDA Functions
+
+    // CUDA: Accessing nextLevelNodes_cuda
+    cudaMalloc (&nextLevelNodes_cuda, numCurrLevelNodes * sizeof(int));
+    cudaMemcpy(nextLevelNodes_cuda, nextLevelNodes, numCurrLevelNodes * sizeof(int), cudaMemcpyHostToDevice);
+    // CUDA: Accessing currLevelNodes_cuda
+    cudaMalloc (&currLevelNodes_cuda, numCurrLevelNodes * sizeof(int));
+    cudaMemcpy(currLevelNodes_cuda, currLevelNodes, numCurrLevelNodes * sizeof(int), cudaMemcpyHostToDevice);
+    // CUDA: Accessing nodeNeighbors_cuda
+    cudaMalloc (&nodeNeighbors_cuda, countNodeNeighbors * sizeof(int));
+    cudaMemcpy(nodeNeighbors_cuda, nodeNeighbors, countNodeNeighbors * sizeof(int), cudaMemcpyHostToDevice);
+    // CUDA: Accessing nodePtrs_cuda
     cudaMalloc (&nodePtrs_cuda, numNodePtrs * sizeof(int));
-    cudaMemcpy(nodePtrs_cuda, nodePtrs_h, numNodePtrs * sizeof(int), cudaMemcpyHostToDevice);
-
-    int* nodeVisited_cuda = (int*)malloc(numNodesSize) ; 
+    cudaMemcpy(nodePtrs_cuda, nodePtrs, numNodePtrs * sizeof(int), cudaMemcpyHostToDevice);
+    // CUDA: Accessing nodeVisited_cuda
     cudaMalloc (&nodeVisited_cuda, numNodesSize);
-    cudaMemcpy(nodeVisited_cuda, nodeVisited_h,numNodesSize, cudaMemcpyHostToDevice);
-
-    int* nodeInput_cuda = (int*)malloc(numNodesSize) ; 
+    cudaMemcpy(nodeVisited_cuda, nodeVisited,numNodesSize, cudaMemcpyHostToDevice);
+    // CUDA: Accessing nodeInput_cuda
     cudaMalloc (&nodeInput_cuda, numNodesSize);
-    cudaMemcpy(nodeInput_cuda, nodeInput_h, numNodesSize, cudaMemcpyHostToDevice);
-
-    int* nodeOutput_cuda = (int*)malloc(numNodesSize) ; 
+    cudaMemcpy(nodeInput_cuda, nodeInput, numNodesSize, cudaMemcpyHostToDevice);
+    // CUDA: Accessing nodeOutput_cuda
     cudaMalloc (&nodeOutput_cuda, numNodesSize);
-    cudaMemcpy(nodeOutput_cuda, nodeOutput_h, numNodesSize, cudaMemcpyHostToDevice);
-
-    int* nodeGate_cuda = (int*)malloc(numNodesSize) ; 
+    cudaMemcpy(nodeOutput_cuda, nodeOutput, numNodesSize, cudaMemcpyHostToDevice);
+    // CUDA: Accessing nodeGate_cuda
     cudaMalloc (&nodeGate_cuda, numNodesSize);
-    cudaMemcpy(nodeGate_cuda, nodeGate_h, numNodesSize, cudaMemcpyHostToDevice);
+    cudaMemcpy(nodeGate_cuda, nodeGate, numNodesSize, cudaMemcpyHostToDevice);
 
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // step 4: time parallel execution
@@ -214,84 +203,77 @@ int main(int argc, char *argv[]){
     // step 5: write to file and done!
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    int* outputBuffer;
-    outputBuffer = (int*)malloc(numNodesSize);
-    cudaMemcpy(outputBuffer, nodeOutput_cuda, numNodesSize, cudaMemcpyDeviceToHost);
+    int* output_buff;
+    output_buff = (int*)malloc(numNodesSize);
+    cudaMemcpy(output_buff, nodeOutput_cuda, numNodesSize, cudaMemcpyDeviceToHost);
 
-    cudaMemcpyFromSymbol(&numNextLevelNodes_h, numNextLevelNodes, sizeof(int), 0, cudaMemcpyDeviceToHost);
-    cudaMemcpyFromSymbol(nextLevelNodes_h, nextLevelNodesQueue, numNextLevelNodes_h * sizeof(int), 0, cudaMemcpyDeviceToHost);
+    cudaMemcpyFromSymbol(&numNextLevelNodes, numNextLevelNodes, sizeof(int), 0, cudaMemcpyDeviceToHost);
+    cudaMemcpyFromSymbol(nextLevelNodes, nextLevelNodesQueue, numNextLevelNodes * sizeof(int), 0, cudaMemcpyDeviceToHost);
 
-    write_data(outputBuffer, numNodes, nodeOutputFilename);
-    write_data(nextLevelNodes_h, numNextLevelNodes_h, nextLevelNodesFilename);
+    // Writing data to output files
+    write_data(output_buff, countNodes, output_node); //ouput_node
+    write_data(nextLevelNodes, numNextLevelNodes, output_nln); //output_NodeLevelNode
 
     // ~~~~~~~~~~~~~~~~~~~~~
     // step 6: free at last!
     // ~~~~~~~~~~~~~~~~~~~~~
-
-    free(outputBuffer);
-    free(nextLevelNodes_h);
-    cudaFree(nextLevelNodes_cuda);
-    cudaFree(currLevelNodes_cuda);
-    cudaFree(nodeNeighbors_cuda);
-    cudaFree(nodePtrs_cuda);
-    cudaFree(nodeVisited_cuda);
-    cudaFree(nodeInput_cuda);
-    cudaFree(nodeOutput_cuda);
-    cudaFree(nodeGate_cuda);
+    free(output_buff); free(nextLevelNodes);
+    cudaFree(nextLevelNodes_cuda); cudaFree(currLevelNodes_cuda); cudaFree(nodeNeighbors_cuda); 
+    cudaFree(nodePtrs_cuda); cudaFree(nodeVisited_cuda); cudaFree(nodeInput_cuda);
+    cudaFree(nodeOutput_cuda); cudaFree(nodeGate_cuda);
 }
 
-int read_input_one_two_four(int** input1, char* filepath) {
-    FILE* fp = fopen(filepath, "r");
-    if (fp == NULL) {
-        fprintf(stderr, "Couldn't open file for reading\n");
-        exit(1);
-    }
+int read_input(int** input_vals, char* path_inputfile) {
+  int size_file;
+  int read_value;
 
-    int counter = 0;
-    int len;
-    int length = fscanf(fp, "%d", &len);
-    *input1 = (int*)malloc(len * sizeof(int));
+  FILE* read_this;
+  
+  //File Open
+  if ((read_this = fopen(path_inputfile, "r")) == NULL){
+      printf("Error: Cannot Open File!");
+      exit(1);
+  } 
 
-    int temp1;
+  fscanf(read_this, "%d", &size_file);
+  *input_vals = (int*) malloc(size_file * sizeof(int));
 
-    while (fscanf(fp, "%d", &temp1) == 1) {
-        (*input1)[counter] = temp1;
-        counter++;
-    }
+  //Reading input file values
+  for (int i = 0; i < size_file; i++) {
+      fscanf(read_this, "%d", &read_value);
+      (*input_vals)[i] = read_value;
+  }
 
-    fclose(fp);
-    return len;
+  fclose(read_this);
+  return size_file;
 }
 
-int read_input_three(int** input1, int** input2, int** input3, int** input4, char* filepath) {
-    FILE* fp = fopen(filepath, "r");
-    if (fp == NULL) {
-        fprintf(stderr, "Couldn't open file for reading\n");
-        exit(1);
-    }
+int read_third_file(char* path_inputfile, int** first_input, int** second_input, int** third_input, int** fourth_input) {
+  int size_file;
+  int read_value_1; int read_value_2; int read_value_3; int read_value_4;
+  FILE* read_this;
+  
+  //File Open
+  if ((read_this = fopen(path_inputfile, "r")) == NULL){
+      printf("Error: Cannot Open File!");
+      exit(1);
+  } 
 
-    int counter = 0;
-    int len;
-    int length = fscanf(fp, "%d", &len);
-    *input1 = (int*)malloc(len * sizeof(int));
-    *input2 = (int*)malloc(len * sizeof(int));
-    *input3 = (int*)malloc(len * sizeof(int));
-    *input4 = (int*)malloc(len * sizeof(int));
+  //Allocating memory for input variables
+  fscanf(read_this, "%d", &size_file);
+  *first_input = (int*) malloc(size_file * sizeof(int)); *second_input = (int*) malloc(size_file * sizeof(int));
+  *third_input = (int*) malloc(size_file * sizeof(int)); *fourth_input = (int*) malloc(size_file * sizeof(int));
 
-    int temp1;
-    int temp2;
-    int temp3;
-    int temp4;
-    while (fscanf(fp, "%d,%d,%d,%d", &temp1, &temp2, &temp3, &temp4) == 4) {
-        (*input1)[counter] = temp1;
-        (*input2)[counter] = temp2;
-        (*input3)[counter] = temp3;
-        (*input4)[counter] = temp4;
-        counter++;
-    }
+  //Iterating through all four read values
+  for (int i = 0; i < size_file; i++) {
+      fscanf(read_this, "%d, %d, %d, %d", &read_value_1, &read_value_2, &read_value_3, &read_value_4);
+      //Assigning read values to corresponding input variables
+      (*first_input)[i] = read_value_1; (*second_input)[i] = read_value_2;
+      (*third_input)[i] = read_value_3; (*fourth_input)[i] = read_value_4;
+  }
 
-    fclose(fp);
-    return len;
+  fclose(read_this);
+  return size_file;
 }
 
 void write_data(int* data, int length, char* filepath) {
